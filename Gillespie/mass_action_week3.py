0033b592bb20a5de03d5e1a5d4ae6549f895b17f
Marcus Strauss <marcus@eduroam-169-233-232-169.ucsc.edu>
2019-04-26 16:58:42 -0700

    made modifications for graph

--- Gillespie/mass_action_week3.py
+++ Gillespie/mass_action_week3.py
@@ -11,6 +11,7 @@ import random
 # x5 = LACI*
 # x6 = PLAC*
 # * = inactivated form
+
 parser = argparse.ArgumentParser(description = 'arguments to stimulate LACI transcription factor')
 parser.add_argument('-r', '--reaction', type = float, help = 'reaction rate for mass action')
 parser.add_argument('-f', '--finish', type = int, help = 'Finish time for reaction stimulation')
@@ -28,7 +29,7 @@ def get_args():
     if args.finish:
         finish = args.finish
 
-def stimulate(finish, rr, x1 = 5, x2 = 8, x5 = 1):
+def stimulate(finish, rr, x1 = .25, x2 = .25, x5 = .5):
     # x1 mass reaction
     # dx1/dt = -r1[x1][x2] + r1[x5]
 
@@ -56,11 +57,9 @@ def stimulate(finish, rr, x1 = 5, x2 = 8, x5 = 1):
 
 
         change_x1.append((start, x1)) # change is a list with tuple of time and change in x1
-        start += 1
+        start += .1
 
     return change_x1
-
-
 
 def main():
 
@@ -70,27 +69,14 @@ def main():
 
     fig = plt.figure()  # set up plot
 
-    #plt.axis([0, finish, -10, 10]) #x-min, x-max, y-min, y-max
     plt.xlabel('time')
     plt.ylabel('X')
 
-    for time in range(finish):
-        print(time, data[time][1])
-        plt.scatter(time, data[time][1],
+    for time, value in data:
+        plt.scatter(time, value,
                 s=1, c='black')
 
     plt.savefig("mass_action.png")  # save plot to disk
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
