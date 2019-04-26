ddf86c8301cfc6efb6b2a884b4f7bd3cdd196671
Marcus Strauss <marcus@eduroam-169-233-232-169.ucsc.edu>
2019-04-26 12:28:54 -0700

    close to plotting

--- Gillespie/mass_action_week3.py
+++ Gillespie/mass_action_week3.py
@@ -1,5 +1,7 @@
 import math
 import argparse
+import matplotlib.pyplot as plt
+import random
 
 ####### VARIABLES #######
 # x1 = LACI
@@ -9,22 +11,54 @@ import argparse
 # x5 = LACI*
 # x6 = PLAC*
 # * = inactivated form
-help_str = 'reaction constant for mass action'
-parser = argparse.ArgumentParser(description = 'reaction rates')
-parser.add_argument('-r', '--reaction', type = float, help = help_str, nargs= '+')
+parser = argparse.ArgumentParser(description = 'arguments to stimulate LACI transcription factor')
+parser.add_argument('-r', '--reaction', type = float, help = 'reaction rate for mass action')
+parser.add_argument('-f', '--finish', type = int, help = 'Finish time for reaction stimulation')
 args = parser.parse_args()
 
-
-# makes sure we only have 4 reaction coefficients
-assert len(args.reaction) <=4 , 'we only have 4 reactions'
-
 # setting default reaction rates
-r1 = r2 = r3 = r4 = float()
+r1 = 1.0; finish = 100
 
-if args.reaction:
-    r1, r2, r3, r4 = args.reaction
-else:
-    r1, r2, r3, r4 = [1.0] * 4
+# start concentration for x1, x2, and x5
+x1 = 50; x2 = 100; x5 = 1
+
+def get_args():
+    if args.reaction:
+        r1 = args.reaction
+    if args.finish:
+        finish = args.finish
+
+def stimulate(finish, rr, x1 = 50, x2 = 100, x5 = 1):
+    # x1 mass reaction
+    #  dx1/dt = -r1[x1][x2] + r1[x5]
+    start = 0
+    change_x1 = []
+    while start < finish:
+        rR = bool(random.getrandbits(1))
+
+        if rR:
+            dx1 = -rr * x1 * x2
+        else:
+            dx1 = rr * x5
+
+        x1 += x1 + dx1
+        change_x1.append((start , x1)) # change is a list with tuple of time and change in x1
+        start += 1
+
+    return change_x1
+
+
+
+def main():
+
+    get_args() # grabs argparse args
+    data = stimulate(finish, r1)
+
+    fig = plt.figure()  # set up plot
+
+    plt.axis([0, finish, 0, 10])
+    plt.xlabel('time')
+    plt.ylabel('X')
 
 
 
@@ -32,3 +66,10 @@ else:
 
 
 
+
+
+
+
+
+if __name__ == '__main__':
+    main()
