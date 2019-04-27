894001657d231cc9b2d43a45ac8897cbe80cee38
Marcus Strauss <marcus@eduroam-169-233-232-169.ucsc.edu>
2019-04-26 17:05:33 -0700

    just need to implement QSSA

--- Gillespie/mass_action_week3.py
+++ Gillespie/mass_action_week3.py
@@ -1,9 +1,9 @@
-import math
 import argparse
 import matplotlib.pyplot as plt
 import random
+from typing import List
 
-####### VARIABLES #######
+####### VARIABLES #########
 # x1 = LACI
 # x2 = IPTG
 # x3  = PLAC
@@ -12,6 +12,7 @@ import random
 # x6 = PLAC*
 # * = inactivated form
 
+######### PARSER ###############
 parser = argparse.ArgumentParser(description = 'arguments to stimulate LACI transcription factor')
 parser.add_argument('-r', '--reaction', type = float, help = 'reaction rate for mass action')
 parser.add_argument('-f', '--finish', type = int, help = 'Finish time for reaction stimulation')
@@ -23,16 +24,15 @@ r1 = 1.0; finish = 100
 # start concentration for x1, x2, and x5
 # x1 = 5; x2 = 8; x5 = 1
 
-def get_args():
+def get_args() -> None:
     if args.reaction:
         r1 = args.reaction
     if args.finish:
         finish = args.finish
 
-def stimulate(finish, rr, x1 = .25, x2 = .25, x5 = .5):
+def stimulate_original_mass_action(finish:int, rr:float, x1 = .25, x2 = .25, x5 = .5) -> List:
     # x1 mass reaction
     # dx1/dt = -r1[x1][x2] + r1[x5]
-
     start = 0
     change_x1 = []
     while start < finish:
@@ -40,32 +40,27 @@ def stimulate(finish, rr, x1 = .25, x2 = .25, x5 = .5):
         rR = bool(random.getrandbits(1))
 
         if rR:
-
-            #x1 -= 1; x2 -= 1
-            #x5 += 1
             dx1 = -rr * x1 * x2
             x1 += dx1
 
         else:
-
-            #x1 +=1 ; x2 += 1
-            #x5 -= 1
-
             dx1 = rr * x5
             x1 += dx1
 
-
-
         change_x1.append((start, x1)) # change is a list with tuple of time and change in x1
         start += .1
-
     return change_x1
+
+def stimulate_QSSA():
+    #todo get QSSA working should be easy
+    pass
+
 
 def main():
 
     get_args() # grabs argparse args
 
-    data = stimulate(finish, r1)
+    data = stimulate_original_mass_action(finish, r1)
 
     fig = plt.figure()  # set up plot
 
@@ -77,6 +72,5 @@ def main():
                 s=1, c='black')
 
     plt.savefig("mass_action.png")  # save plot to disk
-
 if __name__ == '__main__':
     main()
