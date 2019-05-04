d125b794d2abafd90cd306e3b7bef9fee283d6a4
Marcus Strauss <marcus@Animus.local>
2019-05-04 16:33:22 -0700

    updated week3

--- massAction/week3.py
+++ massAction/week3.py
@@ -18,50 +18,77 @@ one then attempts to gain information about solutions of this equation;
 and one then translates this mathematical information back into the
 scientific context.
 
+STATE DEFINITIONS:
+
+x2 = repressor (LacI)
+x2* = inactivated repressor
+x3 = IPTG
+x4 = promoter
+x4* = bound promoter
+x6 = mRNA
+
+MASS ACTION EQUATIONS
+R = reaction reate
+X2 + X4 ->(r1) <-(r2) x4* 
+X2 + X3 -> (r3) <- (r4) X2*
+X4 -> (r5) X4 + X6
+X6 -> (r6) ø
+
 """
 
-r1 = 2; r2 = 2; r3 = 2  # rates of reaction
+def differentials(state, t) -> list:
+    r1 = .5; r2 = .1; r3 = .1; r4 = .7; r5 = .3; r6 = .2  # rates of reaction
+    x2, x2Star, x3, x4, x4Star, x6 = state
 
-x1 = 10; x2 = 10; x3 = 10; x5 = 5; x4 =   # concentrations of repressor protein
+    dx2dt = (-r1*x2*x4) + (r2 * x4Star) - (r2 * x2 * x3 ) + (r4 * x4Star)
 
-def dx_x2(x2, t) -> float:
-    # dx2/dt = dx3/dt
-    # IPTG  and inactivated repressor protein respectively
-    # models IPTG (x3) attatching to repressor protein (x2) and going to inactivated form (x5)
-    dx2dt = -r1 * x2 * x3 + r2 * x5
-    return dx2dt
+    dx3dt = -(r5*x2*x3) + (r4*x2Star)
 
+    dx4dt = -(r1 * x2 * x4) + (r2 * x4Star)
 
-def dx_x5(x5, t):
-    # models concentratino of inactives repressor protein (x5)
-    # dependent on x2 (repressor protein) x3 (IPTG)
-    # decay of deactivated repressor proetin (x5)
-    #decay
-    dx5dt = (r1 * x2 * x3) - (r2 * x5 )
+    dx2Stardt = (r3 * x2 * x3) - (r4 * x2Star)
 
-    return dx5dt
+    dx4Stardt = (r1*x2 * x4) - r2 * x4Star
 
-def mRNA():
-    pass
+    dx6dt = (r5 * x4) - (r6 * x6)
 
+    return [dx2dt, dx3dt, dx4dt, dx2Stardt, dx4Stardt, dx6dt]
 
-funcs = [(dx_x2,'Graph_dx3_dx2.png'),
-         (dx_x5, 'Graph_dx5'), ]
+def QSSA(state, t) -> list:
+    r1 = .7; r2 = 1; r3 = .7; r4 = 1; r5 = 4; r6 = 10  # rates of reaction
+    x2, x2Star, x3, x4, x4Star, x6 = state
+
+    # assume dx4dt as 0
+
+    dx2dt = (-r1*x2*x4) + (r2 * x4Star) - (r2 * x2 * x3 ) + (r4 * x4Star)
+
+    dx3dt = -(r5*x2*x3) + (r4*x2Star)
+
+    #dx4dt = -(r1 * x2 * x4) + (r2 * x4Star)
+
+    dx4dt = 0
+
+    dx2Stardt = (r3 * x2 * x3) - (r4 * x2Star)
+
+    dx4Stardt = (r1*x2 * x4) - r2 * x4Star
+
+    dx6dt = (r5 * x4) - (r6 * x6)
+
+    return [dx2dt, dx3dt, dx4dt, dx2Stardt, dx4Stardt, dx6dt]
 
 def main():
-    time = np.linspace(0, 100, 1000) # 100 seconds split over 1000 time steps
+    time = np.linspace(0, 100, 50) # 100 seconds split over 1000 time steps
     # args is odeint(function, y(0) right when we start, interval to calculate over
-
+    funcs = [(differentials, 'differentials.png'), (QSSA, 'QSSA.png')]
     for func, name in funcs:
-        y = odeint(func, 5, time)
+        y = odeint(func, [100, 100, 100, 10, 10, 1,], time)
         plt.plot(time,y)
         plt.savefig(name)
         plt.clf()
 
+
 if __name__ == '__main__':
     main()
-
-
 
 """
 #def dx_x6(x1, t):
